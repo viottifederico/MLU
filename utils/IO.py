876d437b25b338bb03b16314236a8a0134fdf437
Federico Viotti <federico.viotti@sisal.it>
2023-10-25 09:35:43 +0200

    push

--- utils/IO.py
+++ utils/IO.py
@@ -2,6 +2,11 @@
 # - read write files from bucket
 # - perform queries against delta lake
 
+import delta
+from delta import DeltaTable
+from pyspark.sql import SparkSession
+
+
 def load_file_from_bucket(bucket_name, namespace, filename):
     """_summary_
 
@@ -28,14 +33,41 @@ def write_file_in_bucket(dataset_to_write, bucket_name, namespace, filename):
     
     
     
-def query_from_delta(**args):
-    """_summary_
-    """
-    pass
+
+class DeltaLakeUtils:
+    def __init__(self, table_path):
+        self.table_path = table_path
+        self.spark = self._create_spark_session()
+
+    def _create_spark_session(self):
+        return SparkSession.builder \
+            .appName("DeltaLakeUtils") \
+            .getOrCreate()
+
+    def read_table_as_dataframe(self):
+        try:
+            df = self.spark.read.format("delta").load(self.table_path)
+            return df
+        except Exception as e:
+            print(f"Error reading Delta Lake table: {str(e)}")
+            return None
+        
+    def run_sql_query(self, query):
+        try:
+            result = self.spark.sql(query)
+            return result
+        except Exception as e:
+            print(f"Error running Spark SQL query: {str(e)}")
+            return None
+
+    def write_dataframe_to_table(self, dataframe):
+        try:
+            dataframe.write.format("delta").mode("overwrite").save(self.table_path)
+            print(f"Data written to Delta Lake table at {self.table_path}")
+        except Exception as e:
+            print(f"Error writing DataFrame to Delta Lake table: {str(e)}")
+
+    def close(self):
+        self.spark.stop()
 
 
-
-def write_to_delta(**args):
-    """_summary_
-    """
-    pass
